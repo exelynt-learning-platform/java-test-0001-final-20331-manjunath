aeac36bbab2c3e3664539c220e900e98f6dc93b8
Manjunath Yadgiri <105532526+Manjunathy10@users.noreply.github.com>
2026-08-26 10:16:48 +0530

    JAVA TEST 0001

--- HollowDiamondPyramid.java
+++ HollowDiamondPyramid.java
@@ -1,40 +1,39 @@
-import java.util.*;
+public class Main {
+    public static void main(String[] args) {
 
-public class HollowDiamondPyramid {
-
-    public static void printPattern(int n)
-    {
-        int i, j;
-        for (i = 1; i <= n; i++) {
-            for (j = 1; j <= n - i; j++) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2*i-1)
-                    System.out.print("*");
-                else
+            System.out.print("*");
+
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
+                }
+                System.out.print("*");
             }
+
             System.out.println();
         }
-       
-        for (i = n-1; i >= 1; i--) {
-            for (j = 1; j <= n - i; j++) {
+
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-          
-            for (j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2*i-1)
-                    System.out.print("*");
-                else
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
+                }
+                System.out.print("*");
             }
+
             System.out.println();
         }
-    }
-    public static void main(String args[])
-    {
-        int n = 6;
-        printPattern(n);
     }
 }
